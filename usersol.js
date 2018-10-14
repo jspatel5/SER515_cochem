88956ce4dfc9f8cba75bd64898dada2418813e2a
janicejabraham <38218623+janicejabraham@users.noreply.github.com>
2018-10-14 11:01:56 -0700

    Updated the user sol file

--- usersol.js
+++ usersol.js
@@ -1,11 +1,12 @@
-var mysql = require('mysql');
 var http = require('http');
+var bodyParser=require('body-parser');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 var fs = require('fs');
+//var jsonParser = bodyParser.json();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
+var mysql = require('mysql');
 var connection = mysql.createConnection({
 	
 	host :'localhost',
@@ -26,14 +27,14 @@ var server = app.listen(8080, function () {
   console.log("Example app listening at %s : %s Port", host, port)
 
 });
-app.use(function (req, res, next) {
-  //console.log(req.file); // JSON Object
-  next();
-});
 
 
 
 // parse application/json
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
@@ -45,7 +46,7 @@ app.get('/', function(req, res) {
 
 
 
-app.get('/getSolution',urlencodedParser, function (req, res){
+app.post('/getSolution',function (req, res){
 
 
 
@@ -58,28 +59,52 @@ app.get('/getSolution',urlencodedParser, function (req, res){
 
                      console.log("Connected!");
 
-                     console.log(req.body);
-                     var questionID=req.body.questionID
+                     //console.log(req.body);
+                     var questionID=req.body.questionID;
+                     //var questionID=77;
                      console.log(questionID);
                     
 
-                     
-                       var solpath = connection.query('SELECT solutionPath from questions WHERE questionID = ?',[questionID], function (err, result) {
+                      //var solpath = connection.query('SELECT solutionPath from questions WHERE questionID = ?',[questionID], function (err, result) {
+                      //console.log(solpath);
+                      
+                      var solpath;
+                      connection.query('SELECT solutionPath from questions WHERE questionID = ?',[questionID], function (err, rows) {
+    
                        if (err) throw err;
-                       //var tempFile = String(solpath);
-                       var tempFile = "/Users/janiceabraham/Documents/JaniceAbraham_Resume.pdf";
-                       //console.log(tempFile);
+                       
+
+                       /*else{
+                        setValue(rows)
+                       }
+
+                       function setValue(value)
+                       {
+                        solpath=value;
+                        console.log(solpath);
+                       }*/
+                       console.log(rows[0].solutionPath);
+
+                       console.log("hello");
+                       var tempFile = rows[0].solutionPath;
+                       
+
+                       //var tempFile = JSON.stringify(rows);
+                       
+                       console.log(tempFile);
                        fs.readFile(tempFile, function (err,data){
                        res.contentType("application/pdf");
                        res.send(data);
-                    });
+                    
                 
+});
+                       });
 
-                       //});
+                       
 
                             
 
-         });
+         //});
 
 });
 
