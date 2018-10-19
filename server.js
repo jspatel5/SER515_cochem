cdc7853e001b874212bb770b93e7e7a545be6ec1
jspatel5 <jspatel5@asu.edu>
2018-10-19 13:29:05 -0700

    Sprint#2 User Story#14 Task#15,16,17,36 final commit of authentication module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    = require("express");
 var login = require('./routes/loginroutes');
 var bodyParser = require('body-parser');
+var path = require("path");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -10,22 +11,26 @@ app.use(function(req, res, next) {
     next();
 });
 var router = express.Router();
+
 // test route
 router.get('/register', function(req, res) {
-	res.sendFile('registration.html', {root: __dirname });
+	//res.sendFile('registration.html', {root: __dirname });
+	//console.log(__dirname +'/view'+ '/registration.html');
+	res.sendFile(path.join(__dirname,'/view', '/registration.html'));
+	
    // res.json({ message: 'welcome to our upload module apis' });
 });
 router.get('/login', function(req, res) {
-	res.sendFile('login.html', {root: __dirname });
+	res.sendFile(path.join(__dirname,'/view', '/login.html'));
    // res.json({ message: 'welcome to our upload module apis' });
 });
 router.get('/forgotPassword', function(req, res) {
-	res.sendFile('forgotPassword.html', {root: __dirname });
+	res.sendFile(path.join(__dirname,'/view', '/forgotPassword.html'));
    // res.json({ message: 'welcome to our upload module apis' });
 });
 //route to handle user registration
 router.post('/register',login.register);
 router.post('/login',login.login);
-router.post('/forgotPassword',login.forgotPassword);
+//router.post('/forgotPassword',login.forgotPassword);
 app.use('/api', router);
 app.listen(8080);
